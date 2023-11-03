94c4853de682611ee8bfea50773c7a3d41d8cb70
fatenalshim <faten_alshim@yahoo.com>
2023-11-02 19:54:40 -0700

    fix

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
+
+guillaume@ubuntu:~/0x02$ ./100-my_calculator.py ; echo $?
+Usage: ./100-my_calculator.py <a> <operator> <b>
+1
+guillaume@ubuntu:~/0x02$ ./100-my_calculator.py 3 + 5 ; echo $?
+3 + 5 = 8
+0
