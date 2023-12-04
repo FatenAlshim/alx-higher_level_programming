4795418999f482a435447a42cf3a38dc97e4e2e8
FatenAlshim <Faten_alshim@yahoo.com>
2023-12-04 14:25:00 -0800

    T

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-Write a class MyList that inherits from list:
-    *Public instance method: def print_sorted(self): that prints the list,
-    but sorted (ascending sort)
-    *You can assume that all the elements of the list will be of type int
-    *You are not allowed to import any module
+contains the MyList class
 """
 
 
 class MyList(list):
-    """This function print every method of the super class"""
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
+        """prints the sorted list"""
         print(sorted(self))
